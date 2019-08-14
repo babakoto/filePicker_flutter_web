e2cd93922d37fede384c5a112a6b12787c6d831f
babakoto <e.tokiniaina@gmail.com>
2019-08-14 17:20:57 +0200

    init

---
+++ lib/filePicker_flutter_Web/FilePicker.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_web/material.dart';
+
+class FilePicker extends StatefulWidget {
+  @override
+  _FilePickerState createState() => _FilePickerState();
+}
+
+class _FilePickerState extends State<FilePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
